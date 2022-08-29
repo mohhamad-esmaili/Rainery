706228a736ff906228622f2b20786fe8be2a280e
mohhamad-esmaili <esmaili.mohhamad@gmail.com>
2022-08-29 15:23:41 +0430

    edit api url

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -7,7 +7,7 @@ class WeatherService {
   static Future getWeather(double lat, double long) async {
     try {
       var url = Uri.parse(
-          "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$long&hourly=weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,windspeed_10m_max&timezone=auto&past_days=7");
+          "https://api.open-meteo.com/v1/forecast?latitude=$lat&longitude=$long&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,windspeed_10m_max&timezone=auto&past_days=7");
       var response = await http.get(url);
 
       if (response.statusCode == 200) {
@@ -18,7 +18,7 @@ class WeatherService {
 
         return weatherModel;
       } else {
-        Future.error("error");
+        Future.error("error occured in connection");
       }
     } catch (e) {
       rethrow;
